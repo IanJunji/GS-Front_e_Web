97b317b1050aefdc56f4c3c845560103f1e6c9f7
Leandro Kamada <leandrokpesced@gmail.com>
2024-11-21 21:25:03 -0300

    feat carousel functions % example
    functions % carousel html css example

--- GS-Front-Web/js/script.js
+++ GS-Front-Web/js/script.js
@@ -2,9 +2,6 @@ const login_form = document.querySelector('#loginForm');
 const corpo = document.querySelector('.corpo');
 
 
-
-
-
 document.querySelector('#login').addEventListener('click', function () {
     login_form.style.display = 'block';
     corpo.classList.add('blur');
@@ -23,5 +20,23 @@ function submitLogin(){
         window.location.href = "login.html";
         
     }
+}
+
+const images = [
+    'images/carousel/feedback1.png',
+    'images/carousel/feedback2.png',
+    'images/carousel/feedback3.png'
+];
+
+let Index = 0;
+
+function nextImage() {
+    Index = (Index + 1) % images.length;
+    document.getElementById('carousel-img').src = images[Index];
+}
+
+function prevImage() {
+    Index = (Index - 1 + images.length) % images.length;
+    document.getElementById('carousel-img').src = images[Index];
 }
 
