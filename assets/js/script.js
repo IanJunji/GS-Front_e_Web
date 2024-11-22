30e8eca41812b1f279ee1544a09f01ed126163db
Ian Junji <ianmatsushita@gmail.com>
2024-11-22 17:35:29 -0300

    fix - link js

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,7 @@ function submitLogin(){
     }
     else {
         alert ('Login efetuado com sucesso!');
-        window.location.href = "/GS-Front_e_Web/assets/login.html";  
+        window.location.href = "assets/login.html";  
     }
 }
 
